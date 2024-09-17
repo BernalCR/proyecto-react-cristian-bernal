e42294d9c032e189aefa307370934bf9ac1ac06a
Cristian Bernal <bernalcristian2508@gmail.com>
2024-09-17 08:28:25 -0500

    prueba

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
 
 const ItemListContainer = ({greeting}) => {
+
+  let prod = [
+    {nombre: "aaa", precio: 1},
+    {nombre: "bbb", precio: 2},
+    {nombre: "ccc", precio: 3},
+    {nombre: "ddd", precio: 4}
+  ] 
+
+let x = prod.map(p =>(
+  <p>{p.nombre}</p>
+))
+
   return (
-    <h1>{greeting}</h1>
+    <>    
+      <h1>{greeting}</h1>
+    
+      {
+        
+      prod.map(p =>(
+        <p>{p.nombre}</p>
+      ))
+
+      }
+    </>
+
   )
 }
 
